f2aabc9db27fe5e44a5facbb5c6361a44e47b861
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-07-14 13:06:30 +0530

    14july2023

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -15,4 +15,7 @@ export class BetterHighlightDirective implements OnInit{
   @HostListener('mouseenter') mouseover(eventData : Event){
     this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
   }
+  @HostListener('mouseleave') mouseleave(eventData : Event){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+  }
 }
